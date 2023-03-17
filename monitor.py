377f52dc3c7e182990fbda770a2745dcc06f655b
Antony Vasiliev <tony.vasiliev8@gmail.com>
2023-03-17 00:02:27 -0400

    Updated class structure yet again, this time with documentation

--- monitor.py
+++ monitor.py
@@ -6,49 +6,61 @@ from os import path
 import time
 
 class Monitor:
+    # Initialise instance
     def __init__(self, dump_dir, ups_name):
         self.dump_dir = dump_dir
         self.ups_name = ups_name
 
+    # Generate a result object
+    def generate_result(self, success, error, data):
+        return { 'success': success, 'error': str(error), 'data': str(data) }
+
+    # Generate a successful result object
+    def generate_result_success(self, data):
+        return self.generate_result(True, None, data)
+
+    # Generate an error result object
+    def generate_result_error(self, error):
+        return self.generate_result(False, error, None)
+
+    # Poll upsc for UPS status
     def poll(self):
-        result = { 'success': None, 'error': '', 'data': '' }
-
+        # Run the subprocess
         proc = run(['upsc', self.ups_name], stdout=PIPE, stderr=PIPE)
-        result['success'] = proc.returncode == 0
-        result['error'] = proc.stdout.decode('utf-8')
-        result['data'] = proc.stderr.decode('utf-8')
+        
+        # Check exit code of process and generate result
+        if proc.returncode != 0:
+            return self.generate_result_error(proc.stdout.decode('utf-8'))
+        else:
+            return self.generate_result_success(proc.stderr.decode('utf-8'))
 
-        return result
-
+    # Format result data from polling into a nicer output
     def format(self, data):
-        result = { 'success': None, 'error': '', 'data': '' }
+        return self.generate_result_error('Format not yet implemented!')
 
-        result['data'] = data
-
-        return result
-
+    # Write UPS status to dump file
     def write(self, data):
-        result = { 'success': None, 'error': '', 'data': '' }
+        return self.generate_result_error('Write not yet implemented!')
 
-        return result
-
+    # Run iteration of monitor
     def run(self):
-        result = { 'success': None, 'error': '', 'data': '' }
-
+        # Poll status
         poll_result = self.poll()
         if not poll_result['success']:
-            result['success'] = False
-            result['error'] = 'Failed to poll: ' + poll_result['error']
-            return result
+            # Generate error result if failed to poll
+            return self.generate_result_error('Failed to poll: ' + poll_result['error'])
 
+        # Format polled result data
         format_result = self.format(poll_result['data'])
         if not format_result['success']:
-            result['success'] = False
-            result['error'] = 'Failed to format: ' + format_result['error']
-            return result
+            # Generate error result if failed to format
+            return self.generate_result_error('Failed to format: ' + format_result['error'])
 
+        # Write formatted status to dump file
         write_result = self.write(format_result['data'])
         if not write_result['success']:
-            result['success'] = False
-            result['error'] = 'Failed to write: ' + write_result['error']
-            return result
+            # Generate error result if failed to write
+            return self.generate_result_error('Failed to write: ' + write_result['error'])
+
+        # End with a success result
+        return self.generate_result_success('Success!')
